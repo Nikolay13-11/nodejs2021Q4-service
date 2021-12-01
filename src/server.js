d199a25cb6278059b01aec171a575c6b7ffc9239
Nikolay13-11 <mikola13.11@mail.ru>
2021-12-02 01:04:05 +0300

    fix server code

--- src/server.js
+++ src/server.js
@@ -1,34 +1,17 @@
+// const json = require('koa-json');
 const Koa = require('koa')
+
 const bodyParser = require('koa-bodyparser');
 const { PORT } = require('./common/config');
 
 const app = new Koa()
-const HomeRotes = require('./routes/home.router')
+const UserRotes = require('./resources/users/user.router')
 
 app.use(bodyParser())
+// app.use(json());
 
-app.use(HomeRotes.routes())
-.use(HomeRotes.allowedMethods())
+app.use(UserRotes.routes())
+.use(UserRotes.allowedMethods())
 
-app.listen(PORT)
-console.log(`App is running on http://localhost:${PORT}`)
+app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
 
-
-
-// app.listen(PORT, () =>
-//   
-// );
-
-
-// const start = async() => {
-//   try{
-//     await fastify.listen(PORT)
-//   } catch (err) {
-//     fastify.log.error(err)
-//     process.exit(1)
-//   }
-// }
-
-// start()
-
-// fastify.get("/", async() => ('ddddd'))
