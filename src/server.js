2bd9fa76e539cc9e828b1287cf1aeffd19a362fe
Nikolay13-11 <mikola13.11@mail.ru>
2021-12-01 15:54:16 +0300

    add koa to project

--- src/server.js
+++ src/server.js
@@ -1,6 +1,34 @@
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser');
 const { PORT } = require('./common/config');
-const app = require('./app');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const app = new Koa()
+const HomeRotes = require('./routes/home.router')
+
+app.use(bodyParser())
+
+app.use(HomeRotes.routes())
+.use(HomeRotes.allowedMethods())
+
+app.listen(PORT)
+console.log(`App is running on http://localhost:${PORT}`)
+
+
+
+// app.listen(PORT, () =>
+//   
+// );
+
+
+// const start = async() => {
+//   try{
+//     await fastify.listen(PORT)
+//   } catch (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+// }
+
+// start()
+
+// fastify.get("/", async() => ('ddddd'))
