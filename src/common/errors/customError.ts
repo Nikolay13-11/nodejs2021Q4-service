95d5eb23cadd9edd330b1a2cced01a58fbba0b3b
Nikolay13-11 <mikola13.11@mail.ru>
2021-12-26 23:43:58 +0300

    add custom error and handler

---
+++ src/common/errors/customError.ts
@@ -0,0 +1,11 @@
+export class CustomError extends Error {
+    public statusCode: number;
+  
+    public message: string;
+  
+    constructor(statusCode: number, message: string) {
+      super();
+      this.statusCode = statusCode;
+      this.message = message;
+    }
+  }
