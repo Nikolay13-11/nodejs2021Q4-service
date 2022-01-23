2f96bdc191ec0608b1c3aae31e4c41464063f568
Nikolay13-11 <mikola13.11@mail.ru>
2022-01-23 12:38:47 +0300

    fix migration file

--- src/migrations/1642621205750-first.ts
+++ src/migrations/1642621205750-first.ts
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import {MigrationInterface, QueryRunner} from "typeorm";
+import { generateHash } from "../common/middleware/auth";
 
 export class first1642621205750 implements MigrationInterface {
     name = 'first1642621205750'
@@ -10,6 +11,9 @@ export class first1642621205750 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "board" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "columns" json NOT NULL, CONSTRAINT "PK_865a0f2e22c140d261b1df80eb1" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "task" ADD CONSTRAINT "FK_f316d3fe53497d4d8a2957db8b9" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "task" ADD CONSTRAINT "FK_d88edac9d7990145ff6831a7bb3" FOREIGN KEY ("boardId") REFERENCES "board"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+
+
+        await queryRunner.query(`INSERT INTO "user" (name, login, password) VALUES ('admin', 'admin', '${await generateHash("admin")}')`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
